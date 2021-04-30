ed2b6ef3f497bafc015a5b09d150bead0ac57bc4
Alexander Snarsky <snarsky93@gmail.com>
2021-04-30 21:27:16 +0300

    feat: add plusgins gulp & add scss architecture

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,37 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let csso = require('gulp-csso');
+let rename = require("gulp-rename");
+let gcmq = require('gulp-group-css-media-queries');
+let sourcemaps = require("gulp-sourcemaps");
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
+gulp.task('sass', function (){
     return gulp.src('./src/assets/scss/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    // .pipe(csso())
+    .pipe(csso())
     .pipe(autoprefixer({
         cascade: false
     }))
+    .pipe(gcmq())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./src/assets/css'))
-    // .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function() {
-    // browserSync.init({
-    //     server: "./src"
-    // });
+    browserSync.init({
+        server: "./src", 
+        tunnel: 'test',
+    }, 
+    );
 
     gulp.watch('./src/assets/scss/*.scss', gulp.series('sass'));
-    // gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
 });
 
 
