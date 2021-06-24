16fff183d5751a682b88dfb1bebfad4d8fa331ad
Alexander Snarsky <snarsky93@gmail.com>
2021-06-24 17:12:00 +0300

    change watcher js

--- app/assets/js/components/header/index.js
+++ app/assets/js/components/header/index.js
@@ -1,11 +1,1 @@
-const header = document.querySelector('.header'); 
-
-
-document.addEventListener('scroll', () => {
-  if (document.documentElement.scrollTop > 1) {
-    header.classList.add('header_has-bg');
-  } else {
-    header.classList.remove('header_has-bg');
-  }
-}); 
-
+console.log('Hello word - header');
